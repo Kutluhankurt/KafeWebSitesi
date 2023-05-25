c8e6d7478e6927c266e06fe2f15ee5270e4dd10c
Kutluhan <kutluhankurt@outlook.com>
2023-05-25 15:48:00 +0300

    Product backend işlemini yaptım

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -8,7 +8,7 @@ const handler = async (req, res) => {
   if (method === "GET") {
     try {
       const products = await Product.find();
-      res.status(200).json(Product);
+      res.status(200).json(products);
     } catch (err) {
       console.log(err);
     }
